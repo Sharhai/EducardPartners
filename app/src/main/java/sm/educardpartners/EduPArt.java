e1cb34094325ea384c6e32f48f52d99d4ae01c40
Țurcanu Simion <turcanu.simion@yahoo.com>
2017-05-05 00:01:55 +0300

    secondcommit

---
+++ app/src/main/java/sm/educardpartners/EduPArt.java
@@ -0,0 +1,13 @@
+package sm.educardpartners;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EduPArt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edu_part);
+    }
+}
